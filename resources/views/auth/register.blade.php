d71151825377ef7268871cac8c4b7ec2e1fb19a0
tassjani <natassja.adani@gmail.com>
2019-05-13 10:24:52 +0700

    fix register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,6 @@
 @extends('user/app')
+@section('title','Register | WNB Consultant')
+
 
 
 @section('main-content')
@@ -22,64 +24,75 @@
 	</div>
 </div>
 <!-- END Header -->
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Register</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
+<div class="contact-page area-padding">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-sm-6 col-xs-12">
+				<div class="contact-head">
+					<h3>Register Your Account</h3>
+					<p>Create your account here</p>
+					
+				</div>
+			</div>
+			<!-- End contact icon -->
+			<div class="col-md-6 col-sm-6 col-xs-12">
+				<div class="contact-form">
+					<div class="panel-body">
+						@if (count($errors) > 0)
 						<div class="alert alert-danger">
 							<strong>Whoops!</strong> There were some problems with your input.<br><br>
 							<ul>
 								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
+								<li>{{ $error }}</li>
 								@endforeach
 							</ul>
 						</div>
-					@endif
+						@endif
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ route('register') }} ">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+						<form class="form-horizontal" role="form" method="POST" action="{{ route('register') }} ">
+							<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Name</label>
-							<div class="col-md-6">
-								<input type="text" class="form-control" name="name" value="{{ old('name') }}">
+							<div class="form-group">
+								<label class="col-md-4 control-label">Name</label>
+								<div class="col-md-6">
+									<input type="text" class="form-control" name="name" value="{{ old('name') }}">
+								</div>
 							</div>
-						</div>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+							<div class="form-group">
+								<label class="col-md-4 control-label">E-Mail Address</label>
+								<div class="col-md-6">
+									<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+								</div>
 							</div>
-						</div>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
+							<div class="form-group">
+								<label class="col-md-4 control-label">Password</label>
+								<div class="col-md-6">
+									<input type="password" class="form-control" name="password">
+								</div>
 							</div>
-						</div>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Confirm Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password_confirmation">
+							<div class="form-group">
+								<label class="col-md-4 control-label">Confirm Password</label>
+								<div class="col-md-6">
+									<input type="password" class="form-control" name="password_confirmation">
+								</div>
 							</div>
-						</div>
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									Register
-								</button>
+							<div class="form-group">
+								<div class="col-md-6 col-md-offset-4">
+									<button type="submit" class="btn btn-primary">
+										Register
+									</button>
+								</div>
 							</div>
-						</div>
-					</form>
+						</form>
+					</div>
+
 				</div>
 			</div>
+			<!-- End contact Form -->
 		</div>
 	</div>
 </div>
